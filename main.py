89204b4b1a99cbf7ebd3be84d888f116ad0faa30
ab63 <ab63@sanger.ac.uk>
2024-06-20 10:26:44 +0100

    rethink block scope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pysam
 from enum import Enum
-from typing import Optional
 from hairpin import ref2seq
 from statistics import mean, median, stdev
 import argparse
@@ -66,167 +65,165 @@ def main(
             logging.error('vcf rec has no alts')
             continue  # ? ask Peter
 
-        def fill_mut_reads() -> None:
-            alt_test: bool = len(vcf_rec.alts[0]) == 1
-            if vcf_rec.rlen == 1:
-                mut_type = "sub" if alt_test else "ins"
-            elif alt_test:
-                mut_type = "del"
-            else:
-                mut_type = "complex"
-                
-            # check with Peter
-            samples_w_mutants = [name for name in sample_names if vcf_rec.samples[name]["GT"] == (0, 1)]
+        alt_test: bool = len(vcf_rec.alts[0]) == 1
+        if vcf_rec.rlen == 1:
+            mut_type = "sub" if alt_test else "ins"
+        elif alt_test:
+            mut_type = "del"
+        else:
+            mut_type = "complex"
+            
+        # check with Peter
+        samples_w_mutants = [name for name in sample_names if vcf_rec.samples[name]["GT"] == (0, 1)]
 
-            if len(samples_w_mutants) == 0:
-                logging.error('no mutants')
+        if len(samples_w_mutants) == 0:
+            logging.error('no mutants')
+            sys.exit(1)
+        for mut_sample_name in samples_w_mutants:
+            ### get_mutant_reads
+            read_iter, test = tee(bam_reader_dict[mut_sample_name].fetch(vcf_rec.chrom, (vcf_rec.pos - 2), vcf_rec.pos))
+            try:
+                next(test)
+            except StopAsyncIteration:
+                logging.error('empty iterator')
                 sys.exit(1)
-            for mut_sample_name in samples_w_mutants:
-                ### get_mutant_reads
-                read_iter, test = tee(bam_reader_dict[mut_sample_name].fetch(vcf_rec.chrom, (vcf_rec.pos - 2), vcf_rec.pos))
-                try:
-                    next(test)
-                except StopAsyncIteration:
-                    logging.error('empty iterator')
-                    sys.exit(1)
-                for read in read_iter: # type: ignore
-                    if any(x is None for x in [read.query_sequence, read.query_qualities, read.cigarstring, read.reference_start, read.reference_end]):
-                        # breakpoint()
-                        continue  # ?
-                
-                    if not (read.flag & 0x2) or read.flag & 0xE00 or read.mapping_quality < min_mapqual:
-                        # breakpoint()
-                        continue
-                
-                    mut_pos = ref2seq.ref2querypos(read, (vcf_rec.pos - 1)) # VCF 1-INDEXED, BAM 0-INDEXED
-                    mut_op = ref2seq.pos2op(mut_pos, read) if mut_pos != -1 else None
+            for read in read_iter: # type: ignore
+                if any(x is None for x in [read.query_sequence, read.query_qualities, read.cigarstring, read.reference_start, read.reference_end]):
+                    # breakpoint()
+                    continue  # ?
+            
+                if not (read.flag & 0x2) or read.flag & 0xE00 or read.mapping_quality < min_mapqual:
+                    # breakpoint()
+                    continue
+            
+                mut_pos = ref2seq.ref2querypos(read, (vcf_rec.pos - 1)) # VCF 1-INDEXED, BAM 0-INDEXED
+                mut_op = ref2seq.pos2op(mut_pos, read) if mut_pos != -1 else None
 
-                    # Check whether read reports variant or not - skip the rest of the loop if does not report variant
-                    # First, check for sub
-                    # does read.query_x work? or should it be read.query_alignment_x?
-                    if (mut_type == "sub" and
-                        (not (mut_op == Ops.match.value or mut_op == Ops.diff.value) or
-                         read.query_sequence[mut_pos] != vcf_rec.alts[0] or
-                         read.query_qualities[mut_pos] < min_basequal)):
-                        # breakpoint()
-                        continue
-                    # Second, check whether length of read can accommodate size of indel
-                    # what if other alt is longer?
-                    if (mut_pos + vcf_rec.rlen > read.query_length or
-                        mut_pos + len(vcf_rec.alts[0]) > read.query_length):
-                        # breakpoint()
-                        continue
+                # Check whether read reports variant or not - skip the rest of the loop if does not report variant
+                # First, check for sub
+                # does read.query_x work? or should it be read.query_alignment_x?
+                if (mut_type == "sub" and
+                    (not (mut_op == Ops.match.value or mut_op == Ops.diff.value) or
+                     read.query_sequence[mut_pos] != vcf_rec.alts[0] or
+                     read.query_qualities[mut_pos] < min_basequal)):
+                    # breakpoint()
+                    continue
+                # Second, check whether length of read can accommodate size of indel
+                # what if other alt is longer?
+                if (mut_pos + vcf_rec.rlen > read.query_length or
+                    mut_pos + len(vcf_rec.alts[0]) > read.query_length):
+                    # breakpoint()
+                    continue
 
-                    if mut_type == "del":
-                        mut_rng = map(lambda x: ref2seq.ref2querypos(read, x), range((vcf_rec.pos - 1), vcf_rec.pos))  # check with peter re in/exclusivity of range
-                        mut_rng_ops = list(map(lambda x: ref2seq.pos2op(x, read), mut_rng))
-                        if (mut_rng_ops[0] != Ops.match.value or
-                            mut_rng_ops[-1] != Ops.match.value or
-                            any(x != Ops.delete.value for x in mut_rng_ops)):
+                if mut_type == "del":
+                    mut_rng = map(lambda x: ref2seq.ref2querypos(read, x), range((vcf_rec.pos - 1), vcf_rec.pos))  # check with peter re in/exclusivity of range
+                    mut_rng_ops = list(map(lambda x: ref2seq.pos2op(x, read), mut_rng))
+                    if (mut_rng_ops[0] != Ops.match.value or
+                        mut_rng_ops[-1] != Ops.match.value or
+                        any(x != Ops.delete.value for x in mut_rng_ops)):
+                        # breakpoint()
+                        continue
+                elif mut_type == "ins":
+                    mut_rng = map(lambda x: ref2seq.ref2querypos(read, x), range(mut_pos, mut_pos + len(vcf_rec.alts[0]) + 1))
+                    mut_rng_ops = list(map(lambda x: ref2seq.pos2op(x, read), mut_rng))
+                    if (mut_rng_ops[0] != Ops.match.value or
+                        mut_rng_ops[-1] != Ops.match.value or
+                        any(x != Ops.ins.value for x in mut_rng_ops) or
+                        read.query_sequence[mut_pos:len(vcf_rec.alts[0])] != vcf_rec.alts[0]):
+                        # breakpoint()
+                        continue
+            
+                if ('S' in read.cigarstring and  # type: ignore
+                    mean(read.query_alignment_qualities) < clip_qual_cutoff):  # type: ignore
+                    # breakpoint()
+                    continue
+
+                if read.flag & 0x40:  # read first in pair
+                    mut_reads[mut_sample_name].append(read)
+                else:  # read second in pair
+                    read_start = read.reference_start
+                    read_end = read.reference_end
+                    if read.flag & 0x10:
+                        mate = bam_reader_dict[mut_sample_name].mate(read)
+                        if mate.reference_end is None:
                             # breakpoint()
-                            continue
-                    elif mut_type == "ins":
-                        mut_rng = map(lambda x: ref2seq.ref2querypos(read, x), range(mut_pos, mut_pos + len(vcf_rec.alts[0]) + 1))
-                        mut_rng_ops = list(map(lambda x: ref2seq.pos2op(x, read), mut_rng))
-                        if (mut_rng_ops[0] != Ops.match.value or
-                            mut_rng_ops[-1] != Ops.match.value or
-                            any(x != Ops.ins.value for x in mut_rng_ops) or
-                            read.query_sequence[mut_pos:len(vcf_rec.alts[0])] != vcf_rec.alts[0]):
-                            # breakpoint()
-                            continue
-                
-                    if ('S' in read.cigarstring and  # type: ignore
-                        mean(read.query_alignment_qualities) < clip_qual_cutoff):  # type: ignore
-                        # breakpoint()
-                        continue
-
-                    if read.flag & 0x40:  # read first in pair
+                            continue  # ?
+                        if read.reference_start <= mate.reference_end:
+                            read_start = mate.reference_end + 1
+                    else:
+                        if read.reference_end >= read.next_reference_start:  # check with Peter, does this map on to his code accurately
+                            read_end = read.next_reference_start - 1
+                    # Peter's implemenation comments that this conditional below
+                    # checks if mutant overlaps with first read in pair
+                    # I don't see it, perhaps it refers to the code above
+                    if read_start <= (vcf_rec.pos - 1) <= read_end:
                         mut_reads[mut_sample_name].append(read)
-                    else:  # read second in pair
-                        read_start = read.reference_start
-                        read_end = read.reference_end
-                        if read.flag & 0x10:
-                            mate = bam_reader_dict[mut_sample_name].mate(read)
-                            if mate.reference_end is None:
-                                # breakpoint()
-                                continue  # ?
-                            if read.reference_start <= mate.reference_end:
-                                read_start = mate.reference_end + 1
-                        else:
-                            if read.reference_end >= read.next_reference_start:  # check with Peter, does this map on to his code accurately
-                                read_end = read.next_reference_start - 1
-                        # Peter's implemenation comments that this conditional below
-                        # checks if mutant overlaps with first read in pair
-                        # I don't see it, perhaps it refers to the code above
-                        if read_start <= (vcf_rec.pos - 1) <= read_end:
-                            mut_reads[mut_sample_name].append(read)
-                        else:
-                            # breakpoint() 
-                            continue
-        fill_mut_reads()
+                    else:
+                        # breakpoint() 
+                        continue
+        del(read_iter, test, read, mut_sample_name)  # type: ignore
         # breakpoint()
         if all([x is None for x in mut_reads.values()]):
             logging.error('empty mut_reads')
             sys.exit(1)
         
         ### remove_dups_with_wobble
-        def mut_reads_remove_dups_w_wobble() -> None:
-            for mut_sample_name, reads in mut_reads.items():
-                if len(reads) == 0:
-                    continue
-                # want the start of the record, the end
-                ### start_mate_end_pairs()
-                # incidentally, I suppose hairpin only works for paired data?
-                sorted_ends = []
-                for read in reads:
-                    mate = bam_reader_dict[mut_sample_name].mate(read)
-                
-                    if any(x is None for x in [read.reference_start,
-                                            read.reference_end,
-                                            read.cigartuples,
-                                            mate.reference_start,
-                                            mate.reference_end,
-                                            mate.cigartuples]):
-                        continue  # ?
-                
-                    # this gets pos wrt to alignment against reference
-                    start: int = read.reference_start
-                    end: int = read.reference_end
-                    mate_start: int  = mate.reference_start
-                    mate_end: int  = mate.reference_end
-                
-                    # Peter
-                    # behaviour on cig = None?     
-                    # Julia XAM gets cigar info differently, checking CG:B,I tag
-                    # does this matter?
-                    if read.cigartuples[0][0] == Ops.soft.value:
-                        start -= read.cigartuples[0][1]
-                    if read.cigartuples[-1][0] == Ops.soft.value:
-                        end += read.cigartuples[-1][1]
-                    if mate.cigartuples[0][0] == Ops.soft.value:
-                        mate_start -= mate.cigartuples[0][1]
-                    if mate.cigartuples[-1][0] == Ops.soft.value:
-                        mate_end += mate.cigartuples[-1][1]
-                
-                    # appears mate posns simply aren't assigned if none
-                    sorted_ends.append(sorted([start, end, mate_start, mate_end]))
-                ### end
-                sorted_ends: list[list[int]] = sorted(sorted_ends)  # sort sublists on first element
-                min_ends: list[list[int]] = [sorted_ends.pop(0)]
-                # I dont' fully understand this segment but I think it recapitulates the Julia (ask Peter)
-                i = 1
-                while len(sorted_ends) != 0:
-                    loop_ends: list[int] = sorted_ends.pop(0)
-                    max_spans = map(lambda sublist: max([abs(x - y) for x, y in zip(sublist, loop_ends)]), min_ends)
-                 
-                    if all([x <= max_span for x in max_spans]):
-                        min_ends.append(loop_ends)
-                        reads.pop(i)
-                    else:
-                        min_ends = [loop_ends]
-                    i += 1
-                mut_reads[mut_sample_name] = reads
-        mut_reads_remove_dups_w_wobble()
+        for mut_sample_name, reads in mut_reads.items():
+            if len(reads) == 0:
+                continue
+            # want the start of the record, the end
+            ### start_mate_end_pairs()
+            # incidentally, I suppose hairpin only works for paired data?
+            sorted_ends = []
+            for read in reads:
+                mate = bam_reader_dict[mut_sample_name].mate(read)
+            
+                if any(x is None for x in [read.reference_start,
+                                        read.reference_end,
+                                        read.cigartuples,
+                                        mate.reference_start,
+                                        mate.reference_end,
+                                        mate.cigartuples]):
+                    continue  # ?
+            
+                # this gets pos wrt to alignment against reference
+                start: int = read.reference_start
+                end: int = read.reference_end
+                mate_start: int  = mate.reference_start
+                mate_end: int  = mate.reference_end
+            
+                # Peter
+                # behaviour on cig = None?     
+                # Julia XAM gets cigar info differently, checking CG:B,I tag
+                # does this matter?
+                if read.cigartuples[0][0] == Ops.soft.value:
+                    start -= read.cigartuples[0][1]
+                if read.cigartuples[-1][0] == Ops.soft.value:
+                    end += read.cigartuples[-1][1]
+                if mate.cigartuples[0][0] == Ops.soft.value:
+                    mate_start -= mate.cigartuples[0][1]
+                if mate.cigartuples[-1][0] == Ops.soft.value:
+                    mate_end += mate.cigartuples[-1][1]
+            
+                # appears mate posns simply aren't assigned if none
+                sorted_ends.append(sorted([start, end, mate_start, mate_end]))
+            ### end
+            sorted_ends: list[list[int]] = sorted(sorted_ends)  # sort sublists on first element
+            min_ends: list[list[int]] = [sorted_ends.pop(0)]
+            # I dont' fully understand this segment but I think it recapitulates the Julia (ask Peter)
+            i = 1
+            while len(sorted_ends) != 0:
+                loop_ends: list[int] = sorted_ends.pop(0)
+                max_spans = map(lambda sublist: max([abs(x - y) for x, y in zip(sublist, loop_ends)]), min_ends)
+             
+                if all([x <= max_span for x in max_spans]):
+                    min_ends.append(loop_ends)
+                    reads.pop(i)
+                else:
+                    min_ends = [loop_ends]
+                i += 1
+            mut_reads[mut_sample_name] = reads
+        del(read, reads, mut_sample_name)
         # breakpoint()
     
         ### check hairpin filter
