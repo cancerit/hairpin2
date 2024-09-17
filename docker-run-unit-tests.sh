b5a417a3891312459f197638e82877ec8eb1e15e
ab63 <ab63@sanger.ac.uk>
2024-09-17 10:18:20 +0100

    test CI permissions

--- docker-run-unit-tests.sh
+++ docker-run-unit-tests.sh
@@ -4,7 +4,7 @@ if [ -z ${TEST_DIR} ]; then
     echo "TEST_DIR not set!"
     exit 1
 fi
-PKG_DIR=$(python -c "import os;import crispr_lib_matching;import inspect;print(os.path.dirname(inspect.getfile(crispr_lib_matching)))")
+PKG_DIR=$(python -c "import os;import hairpin2;import inspect;print(os.path.dirname(inspect.getfile(hairpin2)))")
 
 echo "$(python --version)"
 echo "Package source directory: ${PKG_DIR}"
