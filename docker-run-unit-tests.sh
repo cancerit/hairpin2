6d404f6f603020b372f7595071b0a6a3a50939cb
ab63 <ab63@sanger.ac.uk>
2024-10-14 14:54:33 +0000

    fix docker tests

--- docker-run-unit-tests.sh
+++ docker-run-unit-tests.sh
@@ -11,6 +11,9 @@ echo "Package source directory: ${PKG_DIR}"
 
 pip install \
     pytest==8.2.2 \
-    pytest-cov==5.0.0 && \
+    pytest-cov==5.0.0 \
+    faker-biology==0.6.4 \
+    factory-boy==3.3.1 \
+    pysam==0.22 && \
 pytest --cov="${PKG_DIR}" "${TEST_DIR}"
 
