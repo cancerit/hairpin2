2a01fa931a25b998064743c927f238512d592545
ab63 <ab63@sanger.ac.uk>
2025-03-12 14:23:33 +0000

    accidentally included algorithm change in this PR whoops

--- hairpin2/main.py
+++ hairpin2/main.py
@@ -285,9 +285,7 @@ def is_variant_AD(
         ad_filt.code = c.FiltCodes.INSUFFICIENT_READS.value
     else:
         if len(la2ms_f) > min_reads:  # if this, then calculate stats
-            # ok Peter's version had MAD calculated wrong! now fixed
-            med_f = median(la2ms_f)
-            mad_f = median(map(lambda x: abs(x - med_f), la2ms_f))
+            mad_f = max(la2ms_f) - min(la2ms_f)
             sd_f = stdev(la2ms_f)
             if len(la2ms_r) <= min_reads:  # if also this, test
                 if (((sum(near_start_f) / len(near_start_f)) < edge_clustering_threshold) and
@@ -299,8 +297,7 @@ def is_variant_AD(
                     ad_filt.set()
         # the nested if statement here makes the combined condition mutually exclusive with the above
         if len(la2ms_r) > min_reads:
-            med_r = median(la2ms_r)
-            mad_r = median(map(lambda x: abs(x - med_r), la2ms_r))
+            mad_r = max(la2ms_r) - min(la2ms_r)
             sd_r = stdev(la2ms_r)
             if len(la2ms_f) <= min_reads:
                 if (((sum(near_start_r) / len(near_start_r)) < edge_clustering_threshold) and
