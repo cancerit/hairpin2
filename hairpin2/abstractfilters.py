ae6558c38f67adbbc9cd3504e08e8e889bd7fb8f
blex-max <ablex7@gmail.com>
2025-06-26 23:08:44 +0100

    intermediate commit; separating filter construct

---
+++ hairpin2/abstractfilters.py
@@ -0,0 +1,164 @@
+# TODO: give example in docstring?
+"""
+A set of Parent Abstract Base Classes that together completely describe the expected implementation of a scientific test
+for recording FILTER entry for a variant record in a VCF
+
+Heavily leverages static type checking in addition to runtime guarantees. As such, implementation of a new filter is quite verbose.
+The payoff for that verbosity is:
+    - complete static and runtime checking of filter validity, which is helpful when describing many filters, and complex tests
+    - strong guarantees about the implementation of any given filter without needing to know about the underlying logic, making filters very easy to use once defined
+"""
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from typing import ClassVar, Protocol, override, Generic, TypeVar, final, Any
+from collections.abc import Collection, Sequence
+from pysam import AlignedSegment
+from enum import IntEnum, auto, EnumMeta
+from statistics import median
+# pyright: reportExplicitAny=false
+# pyright: reportAny=false
+# pyright: reportUnnecessaryIsInstance=false
+
+class IsDataclass(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, Any]] 
+CodeEnum_T = TypeVar("CodeEnum_T", bound=IntEnum, covariant=True)
+@dataclass(slots=True, frozen=True)
+class FilterResult(ABC, Generic[CodeEnum_T]):
+    """
+    Parent ABC class defining the implementation that must be followed by subclasses intending to hold results of running a `FilterTester.test()` on a variant
+    All filters should use subclasses that inherit from this class to hold their results.
+    Defines and guarantees basic properties that must be shared by all filter results:
+        - a 'getinfo' method for returning a string to report to the INFO field of the VCF record - a basic default is provided, but
+          subclasses probably want to override this.
+        - A basic set of instance variables, which may be extended in a subclass as necessary:
+            - name, a string id for the filter to be used in the VCF FILTER field.
+            - a flag, a boolean indicating if the filter is True/False, or None if untested.
+            - a code, an integer code from a set of possibilities, indicating the basis on which the test has returned True/False, or None if untested.
+        - A class variable, Codes, holding an IntEnum describing the set of possibilites used for the code instance variable.
+
+    Codes allows for runtime checking of values provided to the code instance variable. In other words, the Codes ensures that the code variable
+    may only be set to members of Codes, so you can't set code to something meaningless when testing by accident.
+    This class is generic over T, an IntEnum, such that when a subclass is made it must be made with reference to a specific IntEnum -
+    this is the static equivalent of the runtime checking by Codes. When subclassing, create an appropriate enum of codes and define
+    the subclass like so:
+    ```
+    def XYZCodes(IntEnum):
+        ...
+    ... # decorators if needed
+    class XYZResult(FilterResult[XYZCodes]):
+        Codes: ClassVar[type[XYZCodes]] = XYZCodes
+        ... # the rest of the class body, e.g. further instance variables to be associated with a particular result
+    ```
+    """
+    Codes: ClassVar[EnumMeta]
+    name: str  # for VCF FILTER field
+    flag: bool | None
+    code: CodeEnum_T | None
+
+    def __post_init__(self) -> None:
+        if self.flag and not isinstance(self.flag, bool):
+            raise ValueError('The flag field may only be set to a boolean value or None')
+        if self.code and not isinstance(self.code, self.Codes):
+            raise ValueError('The code field may only be filled by values given in the associated CodeEnum ClassVar, or None')
+        if self.flag and not self.code:
+            raise ValueError('If flag is set a code must be provided')
+
+    @abstractmethod
+    def getinfo(self) -> str | None:
+        """
+        Return basic filter info in a string formatted for use in the VCF INFO field - "<flag>|<code>".
+
+        Each filter must return INFO as it should be formatted for the VCF INFO field, or None if not applicable.
+        Subclasses must override this method to return more specific info.
+        """
+ReadCollection_T = TypeVar("ReadCollection_T", bound=Collection[AlignedSegment])
+FilterParams_T = TypeVar("FilterParams_T", bound=IsDataclass)
+FilterResult_T = TypeVar("FilterResult_T", bound=FilterResult[IntEnum], covariant=True)  # covariant such that a test method that returns a subtype of FilterResult[IntEnum] is accepted where FilterResult[IntEnum] (or FilterResult_T) is expected
+class FilterTester(ABC, Generic[ReadCollection_T, FilterParams_T, FilterResult_T]):
+    # TODO: add explanation as to how the class is generic to the docstring
+    """
+    Parent ABC class to be inherited from when implementing a filter test on read data for a variant.
+    Contains a single abstract class method, `test()`, that must be overridden by subclasses for inidvidual filters.
+    This method enforces a signature which:
+        - defines a common interface to filter tests
+        - respects the "Livkov Substitution Principle", so you don't need to know about the implementation details of a FilterTester to be able to use the `test()` method
+    """
+    @classmethod
+    @abstractmethod
+    def test(
+        cls,
+        variant_reads: ReadCollection_T,
+        params: FilterParams_T
+    ) -> tuple[ReadCollection_T, FilterResult_T]:
+        """
+        Each filter must define a test method via override, respecting the method signature.
+        """
+
+
+# concrete filter
+# TODO: move from this module
+class ALCodes(IntEnum):
+    INSUFFICIENT_READS = 0
+    ON_THRESHOLD = auto()
+@final
+@dataclass(slots=True, frozen=True)
+class ALResult(FilterResult[ALCodes]):
+    Codes: ClassVar[type[ALCodes]] = ALCodes
+    name: str = field(default='ALF', init=False)
+    alt: str
+    avg_as: float | None
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+        if not isinstance(self.alt, str):
+            raise ValueError('The alt field may only be represented by a str')
+        if self.avg_as and not isinstance(self.avg_as, float):
+            raise ValueError("average AS may only be represented by a float")
+
+    @override
+    def getinfo(self) -> str:
+        return f"{self.alt}|{self.flag}|{self.code}|{self.avg_as}"
+@final
+@dataclass
+class ALParams(IsDataclass):
+    alt: str
+    al_thresh: float = 0.93
+@final
+@dataclass
+class ALFilter(FilterTester[Sequence[AlignedSegment], ALParams, ALResult]):  # n.b. you can retain the broadest generic nature by reusing the TypeVar and not specifying a concrete type for any of the type arguments to FilterTester
+    """
+    Describe filter
+    """
+    @classmethod
+    @override
+    def test(
+        cls,
+        variant_reads: Sequence[AlignedSegment],
+        params: ALParams
+    ) -> tuple[Sequence[AlignedSegment], ALResult]:
+        # TODO: runtime checking of variant reads (in parent?) and params
+        if len(variant_reads) < 1:
+            code = ALResult.Codes.INSUFFICIENT_READS
+            fresult = ALResult(None, code, params.alt, None)
+        
+        aln_scores: list[float] = []
+
+        for read in variant_reads:
+            try:
+                aln_scores.append(int(read.get_tag('AS')) / read.query_length)  # pyright: ignore[reportUnknownMemberType, reportUnknownArgumentType]  TODO: look into fixing pysam typing
+            except KeyError:
+                pass
+        if len(aln_scores) != 0:
+            avg_as = median(aln_scores)
+            code = ALResult.Codes.ON_THRESHOLD
+            flag = False
+            if avg_as <= params.al_thresh:
+                flag = True
+            fresult = ALResult(flag, code, params.alt, avg_as)
+        else:
+            code = ALResult.Codes.INSUFFICIENT_READS
+            flag = False
+            fresult = ALResult(flag, code, params.alt, None)
+
+        return variant_reads, fresult
+
